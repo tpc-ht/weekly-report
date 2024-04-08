73fd331fc47c33848b0704290986de21f5c4441f
yuuu <59469126+tpc-ht@users.noreply.github.com>
2024-04-08 15:16:58 +0800

    打包方式调整

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+
     routes: [
         { path: "/", component: "index" },
     ],
     npmClient: 'pnpm',
     title: '日报解析',
+    history: {
+        type: 'hash',
+    },
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+
 });
 
 
