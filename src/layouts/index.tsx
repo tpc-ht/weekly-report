21eb7b455039a9cd9daf95eb0eb6c116f8102711
yuuu <59469126+tpc-ht@users.noreply.github.com>
2024-07-09 16:48:16 +0800

    fix: 引用问题修复

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,8 +1,8 @@
-import { Outlet } from "@/.umi/exports";
 import { ConfigProvider } from "antd";
 import locale from "antd/es/locale/zh_CN";
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn"; // 当时不加日期内部的年月没有汉化
+import { Outlet } from "umi";
 dayjs.locale("zh-cn");
 
 export default function Layout() {
